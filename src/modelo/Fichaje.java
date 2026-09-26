7ff7978f7ee0d90a8885fdf8a440dd59fa964842
SergioLpz7 <sergiolpz.2002@gmail.com>
2026-09-26 12:28:26 +0200

    Creo Fichaje y tipoFichaje

--- src/modelo/Fichaje.java
+++ src/modelo/Fichaje.java
@@ -1,4 +1,59 @@
 package modelo;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Fichaje {
+    private String id, idEmpleado;
+    private  LocalDate fecha;
+    private LocalTime hora;
+    private TipoFichaje tipo; 
+
+    public String getId() {
+        return id;
+    }
+
+    public String getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public TipoFichaje getTipo() {
+        return tipo;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setIdEmpleado(String idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public void setTipo(TipoFichaje tipo) {
+        this.tipo = tipo;
+    }
+
+    public Fichaje(String id, String idEmpleado, TipoFichaje tipo) {
+        this.id = id;
+        this.idEmpleado = idEmpleado;
+        this.tipo = tipo;
+        this.fecha = LocalDate.now();
+        this.hora = LocalTime.now(); 
+    }
 }
